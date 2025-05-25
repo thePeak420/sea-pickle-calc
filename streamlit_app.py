287245a71094a7f6284d459e7920d938f3d7b387
thePeak420 <sashrpm@gmail.com>
2025-05-25 17:44:17 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,7 @@ bone_price = st.number_input(
     "Insert the price of each bone", min_value=None, value=0.0, placeholder="Type a number..."
 )
 st.write("The price of each bone is ", bone_price)
+
+money_spent_on_bones = bone_count * bone_price
+
+st.header("You spent ", money_spent_on_bones, " on the bones")
