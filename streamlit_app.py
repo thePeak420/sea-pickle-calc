9e967248baca1ff0fca49a75e3edec3044c0a18d
thePeak420 <sashrpm@gmail.com>
2025-05-25 17:22:53 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+number = st.number_input(
+    "Insert a number", value=None, placeholder="Type a number..."
 )
+st.write("The current number is ", number)
