0fd6d43da412200b9a582fa189d5695cbada621e
thePeak420 <sashrpm@gmail.com>
2025-05-25 17:25:50 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit as st
 bone_count = st.number_input(
     "Insert the number of bones you are ordering", value=0, placeholder="Type a number..."
 )
-st.write("You are ordering ", number, " bones")
+st.write("The amount of bones you ordered is ", number,)
