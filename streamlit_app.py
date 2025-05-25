fc35aefa9046d408e02c4b90177008d1fde0e217
thePeak420 <sashrpm@gmail.com>
2025-05-25 17:39:51 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,8 @@ bone_count = st.number_input(
     "Insert the number of bones you are ordering", min_value=None, value=0, placeholder="Type a number..."
 )
 st.write("The amount of bones you ordered is ", bone_count)
+
+bone_price = st.number_input(
+    "Insert the price of each bone", min_value=None, value=0.0, placeholder="Type a number..."
+)
+st.write("The price of each bone is ", bone_price)
